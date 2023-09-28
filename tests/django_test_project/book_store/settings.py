b1d87c3dcdeaa9703be05d790afb48c59c96be3c
Daniel Roy Greenfeld <pydanny@gmail.com>
2023-09-28 16:01:30 +0100

    Add sample data

--- tests/django_test_project/book_store/settings.py
+++ tests/django_test_project/book_store/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "book_store.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "data.sqlite3",
     }
 }
 
