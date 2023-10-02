4b7503d198615f1e9be7eccea152a9963863cb80
Cody Antunez <cody.antunez@austin.utexas.edu>
2023-10-01 19:13:23 -0500

    Testing if namify converts class name and module into desired format

--- tests/test_dj_notebook.py
+++ tests/test_dj_notebook.py
@@ -5,3 +5,34 @@ def test_thing():
     plus = activate("test_harness")
     # TODO capture STDOUT and assert on it
     assert plus.print() is None
+
+from dj_notebook.shell_plus import DiagramClass
+
+def test_namify():
+    """
+    Test the `namify` method of the `DiagramClass`.
+    
+    Checks if the `namify` method correctly converts the class 
+    name and its module into a format that replaces dots with underscores. 
+    Test covers three scenarios:
+    1. Built-in classes (e.g., `str`).
+    2. Custom classes (e.g., `DiagramClass`).
+    3. Nested classes (e.g., `OuterClass.InnerClass`).
+
+    TODO: Maybe add scenarios of periods included in naming to
+    test conversion
+    """
+    # Create an instance of DiagramClass - include a sample class
+    diagram = DiagramClass(str)
+    
+    # Test built-in class
+    assert diagram.namify(str) == "builtins_str"
+    
+    # Test custom class
+    assert diagram.namify(DiagramClass) == "dj_notebook_shell_plus_DiagramClass"
+    
+    # Test nested class
+    class OuterClass:
+        class InnerClass:
+            pass
+    assert diagram.namify(OuterClass.InnerClass) == "tests_test_dj_notebook_InnerClass"
