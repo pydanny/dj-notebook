fb37e873f1bf1ffa49f98f3f1a1ecfe8d2ca100d
Geoff Beier <geoff@tuxpup.com>
2023-10-20 17:16:51 -0400

    Add `dotenv_file` keyword only parameter to `activate()` and pass it through to `find_django_settings_module`. Clean up imports, squash unused variable warning.

--- src/dj_notebook/__init__.py
+++ src/dj_notebook/__init__.py
@@ -12,12 +12,13 @@ from django_extensions.management import shells
 from IPython.utils.capture import capture_output
 from rich.status import Status
 
-from .config_helper import find_django_settings_module
+from .config_helper import StrPath, find_django_settings_module
 from .shell_plus import Plus
-from dotenv import load_dotenv
 
 
-def activate(settings: str = None, quiet_load: bool = True) -> Plus:
+def activate(
+    settings: str = None, quiet_load: bool = True, *, dotenv_file: StrPath | None = None
+) -> Plus:
     with Status(
         "Loading dj-notebook...\n  Use Plus.print() to see what's been loaded.",
         spinner="bouncingBar",
@@ -26,26 +27,36 @@ def activate(settings: str = None, quiet_load: bool = True) -> Plus:
             # If the caller specified a settings module explicitly, use that
             os.environ["DJANGO_SETTINGS_MODULE"] = settings
         else:
-            source, discovered_settings = find_django_settings_module()
+            source, discovered_settings = find_django_settings_module(
+                dotenv_file=dotenv_file
+            )
             if discovered_settings:
                 if not quiet_load:
-                    print(f"Using {discovered_settings} as DJANGO_SETTINGS_MODULE, discovered from {source}")
+                    print(
+                        f"Using {discovered_settings} as DJANGO_SETTINGS_MODULE, discovered from {source}"
+                    )
                 os.environ["DJANGO_SETTINGS_MODULE"] = discovered_settings
                 try:
-                    spec = importlib.util.find_spec(discovered_settings)
+                    _ = importlib.util.find_spec(discovered_settings)
                 except ModuleNotFoundError:
                     source_path = Path(source)
                     if source.endswith("manage.py") and source_path.is_file():
                         source_dir = Path(source).parent.absolute()
-                        warnings.warn(f"{discovered_settings} from {source} could not be loaded. Adding {str(source_dir)} to search path.")
+                        warnings.warn(
+                            f"{discovered_settings} from {source} could not be loaded. Adding {str(source_dir)} to search path."
+                        )
                         sys.path.append(str(source_dir))
             else:
-                raise ImproperlyConfigured("DJANGO_SETTINGS_MODULE was not specified and could not be discovered.")
+                raise ImproperlyConfigured(
+                    "DJANGO_SETTINGS_MODULE was not specified and could not be discovered."
+                )
         os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
         try:
             django.setup()
         except ModuleNotFoundError as e:
-            raise ImproperlyConfigured(f"DJANGO_SETTINGS_MODULE {e.name} could not be loaded in django.setup()")
+            raise ImproperlyConfigured(
+                f"DJANGO_SETTINGS_MODULE {e.name} could not be loaded in django.setup()"
+            )
 
         with capture_output() as c:
             plus = Plus(shells.import_objects({"quiet_load": False}, no_style()))
